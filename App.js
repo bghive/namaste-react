acbdc0f59fb794db8fcba6bd4adfcf18254beb31
Bakul <bghive09@gmail.com>
2024-03-20 10:51:06 +0530

    episode-2&3

--- App.js
+++ App.js
@@ -1,19 +1,40 @@
+import React from "react";
+import  ReactDOM  from "react-dom/client";
 
-const parent  = React.createElement(
-    "div",{id:"parent"},
-    [React.createElement("div",{id : "child1"},
-       [React.createElement("h1",{},"Hello am from child1 :h1"),
-        React.createElement("h6",{},"Hello am from child1 :h6")]
-    ),
-    React.createElement("div",{id : "child2"},
-       [React.createElement("h1",{},"Hello am from child2 :h1"),
-        React.createElement("h6",{},"Hello am from child2: h6")]
-    )]
 
+const ele = <span>Another element</span>
+
+// Title component
+const title = (
+    <h1 id="heading">
+        {ele}
+        From react element !!!
+        </h1>
+    );
+    
+// Component composition
+const HeadingComponent = () => (
+    <div id="container">
+        {title}
+        <h1>Hi from Functional component!!!</h1>
+    </div>
 );
 
 
 
-//const heading = React.createElement("h1", {id:"heading"}, "Hello World from React !!!");
+// const parent  = React.createElement(
+//     "div",{id:"parent"},
+//     [React.createElement("div",{id : "child1"},
+//        [React.createElement("h1",{},"Hey "),
+//         React.createElement("h6",{},"Hello am from child1 :h6")]
+//     ),
+//     React.createElement("div",{id : "child2"},
+//        [React.createElement("h1",{},"Hello am from child2 :h1"),
+//         React.createElement("h6",{},"Hello am from child2: h6")]
+//     )]
+
+// );
+
+// const heading = React.createElement("h1", {id:"heading"}, "Hello World from React !!!");
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent/>);
